3942f3c7fea22ec7b0499e222deba5b872de5a67
Ishin Tokitsu <toki2222son@yahoo.co.jp>
2019-04-15 01:35:57 +0000

    テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get'talks/top'
-    resources :talks do
-      collection do
-        post :confirm
-      end
-    end 
+  root to: 'talks#top'
+  resources :talks do
+    collection do
+      post :confirm
+    end
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
